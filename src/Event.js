e39fb356a272c76949a10e4e2f1a34d10435562c
Aneri3112 <aneri.pate@hotmail.com>
2022-06-21 19:03:27 +0200

    update styling

--- src/Event.js
+++ src/Event.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Card, CardGroup, Button } from "react-bootstrap";
 import './App.css';
 
 class Event extends Component {
@@ -18,32 +17,33 @@ class Event extends Component {
     const { collapsed } = this.state;
     return (
       <div className="event">
-        <CardGroup>
-          <Card className="event-card">
-            <Card.Title className="summary">{event.summary}</Card.Title>
-            <Card.Subtitle className="start-date"> {event.start.dateTime} ({event.start.timeZone}) </Card.Subtitle>
-            <Card.Subtitle className="location"> @{event.summary} | {event.location} </Card.Subtitle>
+        <h2 className="summary">{event.summary}</h2>
+        <p className="start-date">
+          {event.start.dateTime} ({event.start.timeZone})
+        </p>
 
-            <Button
-              variant="outline-info"
-              className= "details-button"
-              onClick={this.handleClick}
-            >
-              {collapsed ? "Show Details" : "Hide Details"}
-            </Button>
+        <p className="location">
+          @{event.summary} | {event.location}
+        </p>
 
-            {!collapsed && (
-              <div className= "extra-details" >
-              
-                <Card.Title>About the event:</Card.Title>
-                <a href={event.htmlLink} rel="noreferrer" target="_blank">
-                  See details on Google Calendar
-                </a>
-                <p className="event-description">{event.description}</p>
-              </div>
-            )}
-          </Card>
-        </CardGroup>
+        <button
+          variant="outline-info"
+          className= "details-button"
+          onClick={this.handleClick}
+        >
+          {collapsed ? "Show Details" : "Hide Details"}
+        </button>
+
+        {!collapsed && (
+          <div className= "extra-details" >
+          
+            <h3>About the event:</h3>
+            <a href={event.htmlLink} rel="noreferrer" target="_blank">
+              See details on Google Calendar
+            </a>
+            <p className="event-description">{event.description}</p>
+          </div>
+        )}
       </div>
     );
   }
