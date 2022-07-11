3749a4ca7d5e06cc87d3b6f433df55f42930271d
Aneri3112 <aneri.pate@hotmail.com>
2022-07-11 15:06:47 +0200

    syntax error

--- src/api.js
+++ src/api.js
@@ -19,17 +19,13 @@
   
   //if access token found in storage
   export const checkToken = async (accessToken) => {
-    try {
-      const result = await fetch(
-        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-      )
-      return await result.json();
-    }
-    
-    //.then((res) => res.json())
-    catch (error) {
-      error.json();
-    } 
+    const result = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+      .then((res) => res.json())
+      .catch((error) => error.json());
+  
+    return result;
   };
   /**
  * 
